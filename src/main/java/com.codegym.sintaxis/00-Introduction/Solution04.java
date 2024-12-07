1873c375b1e93010d1a03144d930de246c2a81d6
Axel <axeelleguero@gmail.com>
2024-12-07 18:49:20 -0300

    Ejercicio introduccion

---
+++ src/main/java/com.codegym.sintaxis/00-Introduction/Solution04.java
@@ -0,0 +1,17 @@
+public class Solution04 {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 2;
+
+        //System.out.print("dos");
+        System.out.print(b);
+        System.out.print(" más ");
+        //System.out.print(" menos ");
+        System.out.print(a);
+        //System.out.print("tres");
+        System.out.print(" es igual a ");
+        //System.out.print(" igual a ");
+        //System.out.print("cinco");
+        System.out.print(a + b);
+    }
+}

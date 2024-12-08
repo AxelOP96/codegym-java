3c981230ceee1a3acd3b906fc423128f5753fc6f
Axel <axeelleguero@gmail.com>
2024-12-07 21:57:09 -0300

    Ejercicios introduccion java

---
+++ src/main/java/com.codegym.sintaxis/01-JavaIntroduction-output/Solution16.java
@@ -0,0 +1,9 @@
+public class Solution16 {
+    public static void main(String[] args) {
+        System.out.println("¡Amigo es el mejor!");
+
+        System.out.println("¡Amigo" + " es el mejor!");
+
+        System.out.println("¡Amigo" + " " + "es el mejor!");
+    }
+}

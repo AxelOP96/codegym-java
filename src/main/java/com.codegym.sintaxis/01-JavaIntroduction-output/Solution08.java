5f7a81e5f2711bc89ae5a963c3743faf71a96302
Axel <axeelleguero@gmail.com>
2024-12-07 21:02:32 -0300

    Ejercicios introduccion java

---
+++ src/main/java/com.codegym.sintaxis/01-JavaIntroduction-output/Solution08.java
@@ -0,0 +1,8 @@
+public class Solution08 {
+    public static void main(String[] args) {
+        String s1 = "Codificación";
+        String s2 = "en";
+        String s3 = "Java";
+        System.out.println(s1 + " " + s2 + " " + s3);
+    }
+}

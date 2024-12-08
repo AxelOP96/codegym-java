050a139e64ebbe2eb9bd9f53da4978fc7fd47f2e
Axel <axeelleguero@gmail.com>
2024-12-08 00:04:46 -0300

    Ejercicios variables

---
+++ src/main/java/com.codegym.sintaxis/02-JavaIntroduction-variables/Solution02.java
@@ -0,0 +1,10 @@
+public class Solution02 {
+    public static void main(String[] args) {
+        printCircleCircumference(5);
+    }
+
+    public static void printCircleCircumference(int radius) {
+        double pi = 3.14d;
+        return 2 * pi * radius;
+    }
+}

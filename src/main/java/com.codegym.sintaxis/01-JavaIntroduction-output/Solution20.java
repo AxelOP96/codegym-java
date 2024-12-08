7a2566e2cc53115f71e96542039cbfd1ab911c90
Axel <axeelleguero@gmail.com>
2024-12-07 22:21:41 -0300

    Ejercicios introduccion java

---
+++ src/main/java/com.codegym.sintaxis/01-JavaIntroduction-output/Solution20.java
@@ -0,0 +1,10 @@
+public class Solution20 {
+    public static void main(String[] args) {
+        suma(2,2)
+    }
+
+    public static void sumar(int a, int b) {
+        int c = a + b;
+        System.out.print(c);
+    }
+}

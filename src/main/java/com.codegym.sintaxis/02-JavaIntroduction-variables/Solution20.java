78d86623fee16dc4275771bf453a9069fa39e456
Axel <axeelleguero@gmail.com>
2024-12-08 20:59:57 -0300

    Ejercicios variables

---
+++ src/main/java/com.codegym.sintaxis/02-JavaIntroduction-variables/Solution20.java
@@ -0,0 +1,11 @@
+public class Solution20 {
+    public static void print3(String s) {
+        System.out.println(s);
+        System.out.println(s);
+        System.out.println(s);
+    }
+
+    public static void main(String[] args) {
+        print3("I love you!");
+    }
+}

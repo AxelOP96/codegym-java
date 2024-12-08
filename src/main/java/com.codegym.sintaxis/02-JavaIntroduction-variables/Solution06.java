ff2cc28e2ecc61ce9393bb3c1802e53b9435e0e1
Axel <axeelleguero@gmail.com>
2024-12-08 00:52:37 -0300

    Ejercicios variables

---
+++ src/main/java/com.codegym.sintaxis/02-JavaIntroduction-variables/Solution06.java
@@ -0,0 +1,9 @@
+public class Solution06 {
+    public static void main(String[] args) {
+        hackSalary(7000);
+    }
+
+    public static void hackSalary(int a) {
+        System.out.println("Your salary is: " + (a +1000) + " dollars per month.")
+    }
+}

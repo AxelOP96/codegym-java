f830c9703c961c0f25c8c0e2b3680f57c937a55f
Axel <axeelleguero@gmail.com>
2024-12-08 20:59:31 -0300

    Ejercicios variables

---
+++ src/main/java/com.codegym.sintaxis/02-JavaIntroduction-variables/Solution18.java
@@ -0,0 +1,21 @@
+public class Solution18 {
+    public static int min(int a, int b, int c) {
+        int min;
+        if a < b && a < c
+        min = a;
+        else if( b < a && b < c){
+            min = b;
+        }
+        else
+            min = c;
+        return min;
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println(min(1, 2, 3));
+        System.out.println(min(-1, -2, -3));
+        System.out.println(min(3, 5, 3));
+        System.out.println(min(5, 5, 10));
+    }
+
+}

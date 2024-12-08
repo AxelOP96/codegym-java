799e3e0cc65ffc9ca47c3503aeba8a31bbcb0630
Axel <axeelleguero@gmail.com>
2024-12-08 00:54:26 -0300

    Ejercicios variables

---
+++ src/main/java/com.codegym.sintaxis/02-JavaIntroduction-variables/Solution08.java
@@ -0,0 +1,10 @@
+public class Solution08 {
+    public static void main(String[] args) {
+        Cat cata = new Cat();
+        Cat catito = new Cat();
+    }
+
+    public static class Cat {
+
+    }
+}

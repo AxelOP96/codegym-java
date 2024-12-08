3128e8cc08c890acf34d4f914214d48824e4796c
Axel <axeelleguero@gmail.com>
2024-12-07 21:57:34 -0300

    Ejercicios introduccion java

---
+++ src/main/java/com.codegym.sintaxis/01-JavaIntroduction-output/Solution17.java
@@ -0,0 +1,12 @@
+public class Solution17 {
+    public static void main(String[] args) {
+        //String s = "15";
+        int a = 5;
+        //int z = 18;
+        int d = 18;
+        int c = a + d;
+        String b = " es mejor que ";
+
+        System.out.println(a + b + c);
+    }
+}

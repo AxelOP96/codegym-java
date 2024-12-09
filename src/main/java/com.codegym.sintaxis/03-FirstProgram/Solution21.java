8245af573b75a2b0457b636141979cfda5b14cec
Axel <axeelleguero@gmail.com>
2024-12-09 00:39:54 -0300

    Ejercicios programas

---
+++ src/main/java/com.codegym.sintaxis/03-FirstProgram/Solution21.java
@@ -0,0 +1,8 @@
+public class Solution21 {
+    public static void main(String[] args) throws Exception {
+        Scanner teclado = new Scanner(System.in);
+        String name = teclado.nextLine();
+        int number = teclado.nextInt();
+        System.out.println(name + " will take over the world in " + number + " years. Mwa-ha-ha!" );
+    }
+}

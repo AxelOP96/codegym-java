3147a5b9e060376f2b228fe158f7c93c934ffbce
Axel <axeelleguero@gmail.com>
2024-12-09 15:01:17 -0300

    Ejercicios ciclos

---
+++ src/main/java/com.codegym.sintaxis/04-Cicles/Solution02.java
@@ -0,0 +1,17 @@
+public class Solution02 {
+    public static void main(String[] args) {
+        Apple apple = new Apple();
+        apple.addPrice(50);
+        Apple apple2 = new Apple();
+        apple2.addPrice(100);
+        System.out.println("The cost of apples is " + Apple.applePrice);
+    }
+
+    public static class Apple {
+        public static int applePrice = 0;
+
+        public static void addPrice(int applePrice) {
+            Apple.applePrice += applePrice;
+        }
+    }
+}

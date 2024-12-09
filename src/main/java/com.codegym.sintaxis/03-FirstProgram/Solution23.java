b3b1c3444313a1c014a40a99113cbd05ee5fb236
Axel <axeelleguero@gmail.com>
2024-12-09 00:40:33 -0300

    Ejercicios programas

---
+++ src/main/java/com.codegym.sintaxis/03-FirstProgram/Solution23.java
@@ -0,0 +1,7 @@
+public class Solution23 {
+    public static void main(String[] args) throws Exception {
+        Scanner teclado = new Scanner(System.in);
+        String name = teclado.nextLine();
+        System.out.println(name + " makes $120,000 a year. Ha-ha-ha!");
+    }
+}

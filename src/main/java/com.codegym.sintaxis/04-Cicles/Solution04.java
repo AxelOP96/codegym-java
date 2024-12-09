8d63d1d8ae4812160d80bdb5b8cae017210b32c7
Axel <axeelleguero@gmail.com>
2024-12-09 15:01:57 -0300

    Ejercicios ciclos

---
+++ src/main/java/com.codegym.sintaxis/04-Cicles/Solution04.java
@@ -0,0 +1,11 @@
+public class Cat {
+    private static int catCount = 0;
+
+    public static void addNewCat() {
+        Cat.catCount++;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}

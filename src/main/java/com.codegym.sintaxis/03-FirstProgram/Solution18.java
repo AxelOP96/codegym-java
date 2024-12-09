095a35abf010efeb94a02766d6bfce4185c6b7e4
Axel <axeelleguero@gmail.com>
2024-12-09 00:38:53 -0300

    Ejercicios programas

---
+++ src/main/java/com.codegym.sintaxis/03-FirstProgram/Solution18.java
@@ -0,0 +1,8 @@
+public class Solution18 {
+    public static void main(String[] args) {
+        System.out.println("日本語");
+    }
+}
+//BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+//String name = reader.readLine();

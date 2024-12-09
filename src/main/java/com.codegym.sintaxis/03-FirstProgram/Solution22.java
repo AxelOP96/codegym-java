50b37fecf821e82c897e5fb5702b4d42e649545b
Axel <axeelleguero@gmail.com>
2024-12-09 00:40:11 -0300

    Ejercicios programas

---
+++ src/main/java/com.codegym.sintaxis/03-FirstProgram/Solution22.java
@@ -0,0 +1,10 @@
+public class Solution22 {
+    public static void main(String[] args) throws Exception {
+        Scanner teclado = new Scanner(System.in);
+        String name = teclado.nextLine();
+        int num1 = teclado.nextInt();
+        int num2 = teclado.nextInt();
+        System.out.println(name + " will receive " num1 + " in " num2 + " years.");
+    }
+}
+

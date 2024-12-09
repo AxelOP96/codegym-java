d5311f214ea20a44f924b8854f874f0e80eb7917
Axel <axeelleguero@gmail.com>
2024-12-09 11:06:21 -0300

    Ejercicios programas

---
+++ src/main/java/com.codegym.sintaxis/03-FirstProgram/Solution26.java
@@ -0,0 +1,7 @@
+public class Solution26 {
+    public static void main(String[] args) {
+        for(int i=0; i<10;i++){
+            System.out.println("I want a big salary, and that's why I'm studying Java");
+        }
+    }
+}

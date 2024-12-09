b91793d57836429fbd1287df1713c79cb7a0fba6
Axel <axeelleguero@gmail.com>
2024-12-08 22:39:19 -0300

    Ejercicios programas

---
+++ src/main/java/com.codegym.sintaxis/03-FirstProgram/Solution09.java
@@ -0,0 +1,9 @@
+public class Solution09 {
+    public static void main(String[] args) {
+        int result =1;
+        for(int i = 1; i < 11; i++){
+            result *= i;
+        }
+        System.out.println(result);
+    }
+}

2dd076e340dc8e11b0073dff38ebbee8fcbea95b
Axel <axeelleguero@gmail.com>
2024-12-08 22:36:26 -0300

    Ejercicios programas

---
+++ src/main/java/com.codegym.sintaxis/03-FirstProgram/Solution02.java
@@ -0,0 +1,9 @@
+public class Solution02 {
+    public static void printString(String s){
+        System.out.println(s);
+    }
+
+    public static void main(String[] args) {
+        printString("Hello, Amigo!");
+    }
+}

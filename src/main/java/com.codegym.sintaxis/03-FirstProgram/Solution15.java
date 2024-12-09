06213e0a980c10017823392879c0d8ed58de6d81
Axel <axeelleguero@gmail.com>
2024-12-08 23:34:48 -0300

    Ejercicios programas

---
+++ src/main/java/com.codegym.sintaxis/03-FirstProgram/Solution15.java
@@ -0,0 +1,11 @@
+public class Solution15 {
+    public static void main(String[] args) {
+        for(int i = 1; i<=10; i++){
+            String result = "";
+            for(int j=1; j<=10; j++){
+                result= result + (i*j) + " ";
+            }
+            System.out.println(result);
+        }
+    }
+}

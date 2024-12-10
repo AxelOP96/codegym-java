58123095a8f6bfe4b797545a44abcd17cf2caf99
Axel <axeelleguero@gmail.com>
2024-12-09 22:28:25 -0300

    Ejercicios ciclos

---
+++ src/main/java/com.codegym.sintaxis/04-Cicles/Solution28.java
@@ -0,0 +1,25 @@
+public class Solution28 {
+    public static void main(String[] args) throws Exception {
+        Scanner teclado = new Scanner(System.in);
+        int a = teclado.nextInt();
+        int b = teclado.nextInt();
+        int c = teclado.nextInt();
+        int countPos =0;
+        int countNeg
+        if( a > 0)
+            countPos++;
+        else if(b > 0)
+            countPos++;
+        else if(c > 0)
+            countPos++;
+        if( a < 0)
+            countNeg++;
+        else if(b < 0)
+            countNeg++;
+        else if(c < 0)
+            countNeg++;
+        System.out.println("Number of negative numbers: " + countNeg);
+        System.out.println("Number of positive numbers: " + countPos);
+
+    }
+}

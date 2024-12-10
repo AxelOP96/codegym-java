73ae4d71d7ac7fc0426fa98475c7bd6bc635977d
Axel <axeelleguero@gmail.com>
2024-12-09 21:45:55 -0300

    Ejercicios ciclos

---
+++ src/main/java/com.codegym.sintaxis/04-Cicles/Solution20.java
@@ -0,0 +1,15 @@
+public class Solution20 {
+    public static void main(String[] args) throws Exception {
+        Scanner teclado = new Scanner();
+        int a, b, c = teclado.nextInt();
+        if(b == c){
+            System.out.println(1);
+        }
+        else if(a == c){
+            System.out.println(2);
+        }
+        else if(b == a){
+            System.out.println(3);
+        }
+    }
+}

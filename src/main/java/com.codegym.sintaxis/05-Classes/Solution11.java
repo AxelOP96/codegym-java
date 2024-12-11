d00390775a72b891dd2c8623530c049a39d6d34b
Axel <axeelleguero@gmail.com>
2024-12-10 23:47:29 -0300

    Ejercicios clases

---
+++ src/main/java/com.codegym.sintaxis/05-Classes/Solution11.java
@@ -0,0 +1,33 @@
+public class Cat {
+    String Name;
+    int age;
+    int weight;
+    String address;
+    String color;
+    public Cat(String name){
+        this.Name = name;
+    }
+    public Cat(String name, int weight, int age){
+        this.Name = name;
+        this.weight = weight;
+        this.age = age;
+    }
+    public Cat(String name, int age){
+        this.Name = name;
+        this.age = age;
+    }
+    public Cat(int weight, String color){
+
+        this.weight = weight;
+        this.color = color;
+    }
+    public Cat(int weight, String color, String address){
+        this.weight = weight;
+        this.color = color;
+        this.address = address;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}

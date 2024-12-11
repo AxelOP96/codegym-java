67c5041f9ac89423104e09bdd7d18cede7a3fca4
Axel <axeelleguero@gmail.com>
2024-12-10 23:47:36 -0300

    Ejercicios clases

---
+++ src/main/java/com.codegym.sintaxis/05-Classes/Solution12.java
@@ -0,0 +1,23 @@
+public class Dog {
+    String Name;
+    int height;
+    String color;
+    public Dog(String Name){
+        this.Name = Name;
+    }
+    public Dog(String Name, int height){
+        this.Name = Name;
+        this.height = height;
+
+    }
+    public Dog(String Name, int height, String color){
+        this.Name = Name;
+        this.height = height;
+        this.color = color;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+

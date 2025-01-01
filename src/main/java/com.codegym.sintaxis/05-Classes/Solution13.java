68389685fa4bf55c4c28128b550101c58ef8fbb7
Axel <axeelleguero@gmail.com>
2025-01-01 19:34:02 -0300

    Ejercicios clases

---
+++ src/main/java/com.codegym.sintaxis/05-Classes/Solution13.java
@@ -0,0 +1,34 @@
+/*
+* - centerX, centerY, radius
+- centerX, centerY, radius, width
+- centerX, centerY, radius, width, color*/
+public class Circle {
+    //write your code here
+    private int centerX;
+    private int centerY;
+    private int radius;
+    private String color;
+    private int width;
+
+    public Circle(int centerX, int centerY, int radius){
+        this.centerX = centerX;
+        this.centerY = centerY;
+        this.radius = radius;
+    }
+    public Circle(int centerX, int centerY, int radius, int width){
+        this.centerX = centerX;
+        this.centerY = centerY;
+        this.radius = radius;
+        this.width = width;
+    }
+    public Circle(int centerX, int centerY, int radius, int width, String color){
+        this.centerX = centerX;
+        this.centerY = centerY;
+        this.radius = radius;
+        this.width = width;
+        this.color = color;
+    }
+    public static void main(String[] args) {
+
+    }
+}

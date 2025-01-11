9305a9601d8d6e174a9f768a3d0fd05d31e3ba2d
Axel <axeelleguero@gmail.com>
2025-01-11 00:21:40 -0300

    Ejercicios collections

---
+++ src/main/java/com.codegym.sintaxis/08-Collections/Solution12_08.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Solution12_08 {
+    public static List getListForGet() {
+        List<Object> lista = new ArrayList<>();
+        return lista;
+
+    }
+
+    public static List getListForSet() {
+        List<Object> lista = new ArrayList<>();
+        return lista;
+
+    }
+
+    public static List getListForAddOrInsert() {
+        List<Object> lista = new ArrayList<>();
+        return lista;
+
+    }
+
+    public static List getListForRemove() {
+        List<Object> lista = new LinkedList<>();
+        return lista;
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}

c739ab8cdae9321c3b0970def996fff8c2836a59
Axel <axeelleguero@gmail.com>
2025-01-20 01:16:09 -0300

    Refactor ejercicios

--- src/main/java/com.codegym.sintaxis/08-Collections/Solution30_08.java
+++ src/main/java/com.codegym.sintaxis/08-Collections/Solution30_08.java
@@ -1,8 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Solution30_08 {
     /*
@@ -12,24 +11,25 @@ Software update
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        // List of addresses
-        List<String> addresses = new ArrayList<>();
+        HashMap<String, String> addresses = new HashMap<>();
         while (true) {
+            String city = reader.readLine();
+            if (city.isEmpty()) {
+                break;
+            }
             String family = reader.readLine();
             if (family.isEmpty()) {
                 break;
             }
 
-            addresses.add(family);
+
+            addresses.put(city, family);
         }
 
-        // Read the house number
-        //int houseNumber = Integer.parseInt(reader.readLine());
-        String city = reader.readLine();
-        /*if (0 <= houseNumber && houseNumber < addresses.size()) {
-            String familyName = addresses.get(houseNumber);
-            System.out.println(familyName);
-        }*/
+        String answer = reader.readLine();
+        if(addresses.containsKey(answer)){
+            System.out.println(addresses.get(answer));
+        }
 
     }
 }

e5706f6c1b19ecba648464d70491fc70a5d5b7ef
Axel <axeelleguero@gmail.com>
2025-01-24 22:24:17 -0300

    Ejercicios excepciones

---
+++ src/main/java/com.codegym.sintaxis/09-Exceptions/Solution31_09.java
@@ -0,0 +1,68 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/*
+Task about algorithms
+
+*/
+public class Solution31_09 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<String> list = new ArrayList<String>();
+        while (true) {
+            String s = reader.readLine();
+            if (s.isEmpty()) {
+                break;
+            }
+            list.add(s);
+        }
+
+        String[] array = list.toArray(new String[list.size()]);
+        sort(array);
+
+        for (String x : array) {
+            System.out.println(x);
+        }
+    }
+
+    public static void sort(String[] array) {
+        for(int i =0; i< array.length-1; i++){
+            for(int j=0; j< array.length-1; j++){
+                if(isGreaterThan(array[j], array[j+1])){
+                    String aux = array[j+1];
+                    array[j+1] = array[j];
+                    array[j] = aux;
+                }
+            }
+        }
+    }
+
+    // String comparison method: 'a' is greater than 'b'
+    public static boolean isGreaterThan(String a, String b) {
+        return a.compareTo(b) > 0;
+    }
+
+
+    // Is the passed string a number?
+    public static boolean isNumber(String s) {
+        if (s.length() == 0) return false;
+
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            char c = chars[i];
+            if ((i != 0 && c == '-') // The string contains a hyphen
+                    || (!Character.isDigit(c) && c != '-') // or is not a number and doesn't start with a hyphen
+                    || (i == 0 && c == '-' && chars.length == 1)) // or is a single hyphen
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+/*
+Task about algorithms
+
+*/
+}

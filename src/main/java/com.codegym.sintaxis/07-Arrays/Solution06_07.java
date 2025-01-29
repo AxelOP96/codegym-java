c5c9e7de5ea40a4cda44b5399fa15ae17d0118f5
Axel <axeelleguero@gmail.com>
2025-01-29 19:04:42 -0300

    Refactor solucion

--- src/main/java/com.codegym.sintaxis/07-Arrays/Solution06_07.java
+++ src/main/java/com.codegym.sintaxis/07-Arrays/Solution06_07.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Solution06_07 {
+    //Streets and houses
     public static void main(String[] args) throws Exception {
         //write your code here
         int[] array = new int[15];
@@ -10,14 +11,17 @@ public class Solution06_07 {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         for(int i=0; i<array.length;i++){
             array[i] = Integer.parseInt(reader.readLine());
-            if(array[i] %2==0){
-                even++;
+            if(i %2==0){
+                even+=array[i];
             }else{
-                odd++;
+                odd+=array[i];
             }
         }
         if(even < odd){
             System.out.println("Odd-numbered houses have more residents.");
+        }
+        else if(even == odd){
+
         }else{
             System.out.println("Even-numbered houses have more residents.");
         }
--- src/main/java/com.codegym.sintaxis/07-Arrays/Solution30_07.java
+++ src/main/java/com.codegym.sintaxis/07-Arrays/Solution30_07.java
@@ -4,16 +4,32 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class Solution30_07 {
+    //In decreasing order
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        ArrayList<Integer> lista = new ArrayList<>();
+        int[] array = new int[20];
         for (int i = 0; i < 20; i++) {
-            lista.add(Integer.parseInt(reader.readLine())) ;
+            array[i] = Integer.parseInt(reader.readLine());
         }
-        Collections.reverse(lista);
 
-        for (int x : lista) {
+        sort(array);
+
+        for (int x : array) {
             System.out.println(x);
+        }
+    }
+
+    public static void sort(int[] array) {
+        int aux = 0;
+        for(int i = 0; i< array.length-1;i++){
+            for(int j = 0; j< array.length -1; j++){
+                if(array[j+1]> array[j]){
+                    aux = array[j+1];
+                    array[j+1] = array[j];
+                    array[j]= aux;
+
+                }
+            }
         }
     }
 }
